dcb0d475312588dc2c98d7fc0e0d886980082345
Chad Baxter <cbaxter@mail.umw.edu>
2017-04-17 21:25:43 -0500

    added time command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+extern crate chrono;
 extern crate irc;
+
+
+use chrono::Local;
 
 use irc::client::prelude::*;
 
@@ -14,14 +18,17 @@ fn main() {
         print!("{}", message);
         match message.command {
             Command::PRIVMSG(ref target, ref msg) => {
-                if msg.contains("archinbald ping") {
+                if msg.split_at(15).0.contains("@archinbald ping") {
                     server.send_privmsg(target, "I'm right here...oh, sorry...PONG!").unwrap();
                 }
-                if msg.contains("archinbald echo") {
+                if msg.split_at(15).0.contains("@archinbald echo") {
                     server.send_privmsg(target, decorate(msg.split_at(15).1).as_str()).unwrap();
                 }
-                if msg.contains("archinbald info") {
-                    server.send_privmsg(target, "I am Archinbald, your faithful ACM assistant. Currently running v0.1.4. Written in :rust: with :heart: by :logoilab:.").unwrap();
+                if msg.split_at(15).0.contains("@archinbald info") {
+                    server.send_privmsg(target, "I am Archinbald, your faithful ACM assistant. Currently running v0.1.5. Written in :rust: with :heart: by :logoilab:.").unwrap();
+                }
+                if msg.split_at(15).0.contains("@archinbald time") {
+                    server.send_privmsg(target, decorate(Local::now().to_string().as_str()).as_str()).unwrap();
                 }
             },
             _ => (),
