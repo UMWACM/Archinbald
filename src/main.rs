bfa9d72149dd7c4d2d9998427614cf554542d8c3
Chad Baxter <cbaxter@mail.umw.edu>
2017-04-17 19:50:07 -0500

    changed config file type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate irc;
+
+use irc::client::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let server = IrcServer::new("config.json").unwrap();
+    server.identify().unwrap();
+    for message in server.iter() {
+        // Do message processing.
+    }
 }
