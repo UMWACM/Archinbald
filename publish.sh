d536b57619e7042aeac06500019fad79d5bc3217
Chad Baxter <cbaxter@mail.umw.edu>
2017-04-17 22:41:14 -0500

    added remote server publishing script

---
+++ publish.sh
@@ -0,0 +1,4 @@
+cargo build --release;
+rsync -I --progress ./target/release/archinbald cbaxter@cs.umw.edu:~;
+rsync -I --progress config.json cbaxter@cs.umw.edu:~;
+ssh cbaxter@cs.umw.edu 'pkill archinbald; chmod go-rwx config.json; nohup ~/archinbald';
