1477e8adf37e7f7c8019fcf471120e4aef6a05ab
Chad Baxter <cbaxter@mail.umw.edu>
2017-04-17 21:48:13 -0500

    added some more decorators

--- src/main.rs
+++ src/main.rs
@@ -14,20 +14,20 @@ fn main() {
     let server = IrcServer::new("config.json").unwrap();
     server.identify().unwrap();
     for message in server.iter() {
-        let message = message.unwrap(); // We'll just panic if there's an error.
+        let message = message.unwrap();
         print!("{}", message);
         match message.command {
             Command::PRIVMSG(ref target, ref msg) => {
-                if msg.split_at(15).0.contains("@archinbald ping") {
-                    server.send_privmsg(target, "I'm right here...oh, sorry...PONG!").unwrap();
+                if msg.contains("@archinbald ping") {
+                    server.send_privmsg(target, decorate("I'm right here...oh, sorry...PONG!").as_str()).unwrap();
                 }
-                if msg.split_at(15).0.contains("@archinbald echo") {
+                if msg.contains("@archinbald echo") {
                     server.send_privmsg(target, decorate(msg.split_at(15).1).as_str()).unwrap();
                 }
-                if msg.split_at(15).0.contains("@archinbald info") {
-                    server.send_privmsg(target, "I am Archinbald, your faithful ACM assistant. Currently running v0.1.5. Written in :rust: with :heart: by :logoilab:.").unwrap();
+                if msg.contains("@archinbald info") {
+                    server.send_privmsg(target, decorate("I am Archinbald, your faithful ACM assistant. Currently running v0.1.5. Written in :rust: with :heart: by :logoilab:.").as_str()).unwrap();
                 }
-                if msg.split_at(15).0.contains("@archinbald time") {
+                if msg.contains("@archinbald time") {
                     server.send_privmsg(target, decorate(Local::now().to_string().as_str()).as_str()).unwrap();
                 }
             },
